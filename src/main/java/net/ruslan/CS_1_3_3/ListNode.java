2296e05ddd305c730afaff659887126f4b666ad0
imperator-goose <glookeemaska133@gmail.com>
2024-02-24 09:23:27 +0300

    sdg

---
+++ src/main/java/net/ruslan/CS_1_3_3/ListNode.java
@@ -0,0 +1,19 @@
+package net.ruslan.CS_1_3_3;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+
+    }
+}
--- src/main/java/net/ruslan/CS_1_3_3/Solution.java
+++ src/main/java/net/ruslan/CS_1_3_3/Solution.java
@@ -1,47 +1,60 @@
 package net.ruslan.CS_1_3_3;
 
-import java.util.LinkedList;
+import net.ruslan.CS_1_3_3.ListNode;
 
 public class Solution {
-    public static LinkedList<Integer> addTwoNumbers(LinkedList<Integer> l1, LinkedList<Integer> l2) {
-        LinkedList<Integer> result = new LinkedList<>();
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
         int carry = 0;
 
-        while (!l1.isEmpty() || !l2.isEmpty() || carry != 0) {
+        while (l1 != null || l2 != null) {
+            int x;
+            int y;
 
-            int sum = carry;
-
-            if (!l1.isEmpty()) {
-                sum += l1.removeFirst();
+            if (l1 != null) {
+                x = l1.val;
+            } else {
+                x = 0;
             }
 
-            if (!l2.isEmpty()) {
-                sum += l2.removeFirst();
+            if (l2 != null) {
+                y = l2.val;
+            } else {
+                y = 0;
             }
+            int sum = carry + x + y;
 
             carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
 
-            result.add(sum % 10);
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
 
-        return result;
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return head.next;
     }
 
     public static void main(String[] args) {
-        LinkedList<Integer> l1 = new LinkedList<>();
-        l1.add(2);
-        l1.add(4);
-        l1.add(3);
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
 
-        LinkedList<Integer> l2 = new LinkedList<>();
-        l2.add(5);
-        l2.add(6);
-        l2.add(4);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
 
-        LinkedList<Integer> result = addTwoNumbers(l1, l2);
 
-        for (int num : result) {
-            System.out.print(num + " ");
+        ListNode result = addTwoNumbers(l1, l2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
         }
 
     }
